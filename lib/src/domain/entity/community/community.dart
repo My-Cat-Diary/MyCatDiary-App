a85a93c80bc589e78fa8d8752e35f5173d1ad43a
rlarldud1234 <rlarldud6482@naver.com>
2025-05-09 19:52:31 +0900

    new : community entity

---
+++ lib/src/domain/entity/community/community.dart
@@ -0,0 +1,17 @@
+part of '../entity.dart';
+
+@freezed
+abstract class CommunityEntity with _$CommunityEntity {
+  factory CommunityEntity({
+    required int id,
+    required String title,
+    required String content,
+    required String category,
+    required DateTime created,
+    required DateTime updated,
+    required UserEntity user,
+  }) = _CommunityEntity;
+
+  factory CommunityEntity.fromJson(Map<String, dynamic> json) =>
+      _$CommunityEntityFromJson(json);
+}
