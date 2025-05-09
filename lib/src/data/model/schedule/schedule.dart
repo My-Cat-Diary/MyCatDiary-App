18b5c811535785f4b3eb8f188e53399a51761cd9
rlarldud1234 <rlarldud6482@naver.com>
2025-05-09 19:51:51 +0900

    new : schedule model

---
+++ lib/src/data/model/schedule/schedule.dart
@@ -0,0 +1,16 @@
+part of '../model.dart';
+
+@freezed
+abstract class ScheduleModel with _$ScheduleModel {
+  factory ScheduleModel({
+    required int id,
+    required List<String> images,
+    required String title,
+    required String content,
+    required String day,
+    required CatModel cat,
+  }) = _ScheduleModel;
+
+  factory ScheduleModel.fromJson(Map<String, dynamic> json) =>
+      _$ScheduleModelFromJson(json);
+}
