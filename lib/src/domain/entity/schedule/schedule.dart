ba419f2fcc1b4805da1ed58932dea898d46b99a9
rlarldud1234 <rlarldud6482@naver.com>
2025-05-09 19:52:46 +0900

    new : schedule entity

---
+++ lib/src/domain/entity/schedule/schedule.dart
@@ -0,0 +1,15 @@
+part of '../entity.dart';
+
+@freezed
+abstract class ScheduleEntity with _$ScheduleEntity {
+  factory ScheduleEntity(
+      {required int id,
+      required List<String> images,
+      required String title,
+      required String content,
+      required String day,
+      required CatEntity cat}) = _ScheduleEntity;
+
+  factory ScheduleEntity.fromJson(Map<String, dynamic> json) =>
+      _$ScheduleEntityFromJson(json);
+}
