f68d2578a4cf5e3188f79c34b10b7c1e25f8a001
rlarldud1234 <rlarldud6482@naver.com>
2025-05-09 19:51:59 +0900

    new : user model

---
+++ lib/src/data/model/user/user.dart
@@ -0,0 +1,15 @@
+part of '../model.dart';
+
+@freezed
+abstract class UserModel with _$UserModel {
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  factory UserModel({
+    required int userId,
+    required String id,
+    required String name,
+    required String image,
+  }) = _UserModel;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+}
