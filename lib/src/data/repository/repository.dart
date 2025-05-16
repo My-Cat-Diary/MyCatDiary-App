810f158d819441e4892cc47a55ad872a8a4b9307
rlarldud1234 <rlarldud6482@naver.com>
2025-05-16 14:27:50 +0900

    new : repository

---
+++ lib/src/data/repository/repository.dart
@@ -0,0 +1,6 @@
+import 'package:my_cat_diary/src/data/model/model.dart';
+import 'package:my_cat_diary/src/data/source/source.dart';
+import 'package:my_cat_diary/src/domain/entity/entity.dart';
+import 'package:my_cat_diary/src/domain/repository/repository.dart';
+
+part 'cat/cat_repository_impl.dart';
