2760ba9bc347b2412470fc1c7a4696dcf8ea5016
rlarldud1234 <rlarldud6482@naver.com>
2025-05-16 14:30:45 +0900

    new : repository

---
+++ lib/src/domain/repository/repository.dart
@@ -0,0 +1,6 @@
+import 'package:my_cat_diary/src/domain/entity/entity.dart';
+
+part 'cat/cat_repository.dart';
+part 'comment/comment_repository.dart';
+part 'community/community_repository.dart';
+part 'diary/diary_repository.dart';
